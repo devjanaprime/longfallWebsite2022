3fe38bbdbb307c35f124e8a87e39ab8a86dd7d94
devjanaprime <devjana@yahoo.com>
2022-10-20 22:18:59 -0500

    switch music players

--- src/components/MusicRelease/MusicRelease.jsx
+++ src/components/MusicRelease/MusicRelease.jsx
@@ -1,19 +1,29 @@
-function MusicRelease( props ) {
+import { useState } from 'react';
+
+
+let MusicRelease=( props )=>{
+const [ spotify, setSpotify ] = useState( true );
+
   return (
   <div className="App-article">
     <hr />
-      <h1>MusicRelease</h1>
-      <iframe 
-        src="https://open.spotify.com/embed/album/2xiTZMXThFDUYokKsbZH58?utm_source=generator" 
-        width="50%" 
-        height="420"
-      />
+      <h1>Title</h1>
 
-      <iframe 
-        src="https://embed.music.apple.com/us/album/what-we-found/1492961027"
-        width="50%" 
-        height="420"
-      />
+      { spotify
+        ?<div><iframe 
+          src="https://open.spotify.com/embed/album/2xiTZMXThFDUYokKsbZH58?utm_source=generator" 
+          width="50%" 
+          height="420"
+        />
+        <p>Switch to <span onClick={ ()=> setSpotify( !spotify  ) }>Apple Music</span></p></div>
+        :<div className=''><iframe 
+          src="https://embed.music.apple.com/us/album/what-we-found/1492961027"
+          width="50%" 
+          height="420"
+        />
+        <p>Switch to <span onClick={ ()=> setSpotify( !spotify  ) }>Spofity</span></p></div>
+        }
+        <p> Descirption</p>
    </div>
   );
 }
